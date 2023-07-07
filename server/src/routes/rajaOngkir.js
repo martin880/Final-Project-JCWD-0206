e290fc1ca0caae240bbba0ef829903ed6141f656
Sand <novrialsandi@gmail.com>
2023-07-07 11:31:39 +0700

    Update rajaOngkir.js

--- server/src/routes/rajaOngkir.js
+++ server/src/routes/rajaOngkir.js
@@ -4,6 +4,5 @@ const rajaOngkirContorller = require("../controllers").rajaOngkirContorller;
 
 router.get("/province/", rajaOngkirContorller.getAllProvince);
 router.get("/city/", rajaOngkirContorller.getAllCity);
-router.get("/city/:id", rajaOngkirContorller.getCitybyId);
 
 module.exports = router;
